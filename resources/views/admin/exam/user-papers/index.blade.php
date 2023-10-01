42448173325867de22f4d54d40fab3cc53c4da8e
Takshak Tiwari <takshaktiwari@gmail.com>
2023-10-01 20:17:13 +0530

    filter fix

--- resources/views/admin/exam/user-papers/index.blade.php
+++ resources/views/admin/exam/user-papers/index.blade.php
@@ -12,50 +12,58 @@
         ],
     ]" />
 
-    <form action="" class="card">
-        <div class="card-body">
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="form-group">
-                        <label for="">Paper</label>
-                        <select name="paper_id" id="paper_id" class="form-control">
-                            <option value="">-- Select --</option>
-                            @foreach ($papers as $paper)
-                                <option value="{{ $paper->id }}" {{ (request('paper_id') == $paper->id) ? 'selected' : '' }}>
-                                    {{ $paper->title }}
-                                </option>
-                            @endforeach
-                        </select>
+    @if (request('filter'))
+        <form action="" class="card">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label for="">Paper</label>
+                            <select name="paper_id" id="paper_id" class="form-control">
+                                <option value="">-- Select --</option>
+                                @foreach ($papers as $paper)
+                                    <option value="{{ $paper->id }}"
+                                        {{ request('paper_id') == $paper->id ? 'selected' : '' }}>
+                                        {{ $paper->title }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
                     </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="form-group">
-                        <label for="">User</label>
-                        <select name="user_id" id="user_id" class="form-control">
-                            <option value="">-- Select --</option>
-                            @foreach ($users as $user)
-                                <option value="{{ $user->id }}" {{ (request('user_id') == $user->id) ? 'selected' : '' }}>
-                                    {{ $user->name }}
-                                </option>
-                            @endforeach
-                        </select>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label for="">User</label>
+                            <select name="user_id" id="user_id" class="form-control">
+                                <option value="">-- Select --</option>
+                                @foreach ($users as $user)
+                                    <option value="{{ $user->id }}"
+                                        {{ request('user_id') == $user->id ? 'selected' : '' }}>
+                                        {{ $user->name }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
                     </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="form-group">
-                        <label for="">Started On</label>
-                        <input type="date" name="started_on" class="form-control" value="{{ request('started_on') }}">
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label for="">Started On</label>
+                            <input type="date" name="started_on" class="form-control"
+                                value="{{ request('started_on') }}">
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="card-footer">
-            <input type="hidden" name="filter" value="1">
-            <button type="submit" class="btn btn-dark px-4">
-                <i class="fas fa-save"></i> Submit
-            </button>
-        </div>
-    </form>
+            <div class="card-footer">
+                <input type="hidden" name="filter" value="1">
+                <button type="submit" class="btn btn-dark px-4">
+                    <i class="fas fa-save"></i> Submit
+                </button>
+                <a href="{{ url()->current() }}" class="btn btn-danger px-4">
+                    <i class="fas fa-times"></i> Reset
+                </a>
+            </div>
+        </form>
+    @endif
 
     <div class="card shadow-sm">
         <div class="card-body table-responsive">
@@ -78,7 +86,7 @@
                                     {{ $userPaper->paper->title }}
                                 </a>
                             </td>
-                            <td >
+                            <td>
                                 <a href="{{ route('admin.users.show', [$userPaper->user]) }}">
                                     {{ $userPaper->user->name }}
                                 </a>
@@ -91,10 +99,12 @@
                             <td class="text-nowrap">{{ $userPaper->start_at?->format('d-M-Y h:i A') }}</td>
                             <td>{{ $userPaper->questions->where('status', 'answered')->sum('marks') }}</td>
                             <td class="text-nowrap">
-                                <a href="{{ route('admin.exam.user-papers.show', [$userPaper]) }}" class="btn btn-sm btn-info load-circle">
+                                <a href="{{ route('admin.exam.user-papers.show', [$userPaper]) }}"
+                                    class="btn btn-sm btn-info load-circle">
                                     <i class="fas fa-info-circle"></i>
                                 </a>
-                                <a href="{{ route('admin.exam.user-papers.delete', [$userPaper]) }}" class="btn btn-sm btn-danger load-circle">
+                                <a href="{{ route('admin.exam.user-papers.delete', [$userPaper]) }}"
+                                    class="btn btn-sm btn-danger load-circle">
                                     <i class="fas fa-trash"></i>
                                 </a>
                             </td>
