f16ffc861b0a4f83ff8ea3a5bf6a636cac5d126b
Takshak Tiwari <58802422+takshaktiwari@users.noreply.github.com>
2025-11-04 17:07:15 +0530

    Add attributes to check answer correctness

--- src/Models/UserQuestion.php
+++ src/Models/UserQuestion.php
@@ -6,11 +6,26 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class UserQuestion extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    protected function isCorrect(): Attribute
+    {
+        return Attribute::make(
+            get: fn() => $this->user_option_id == $this->correct_option_id
+        );
+    }
+
+    protected function isInCorrect(): Attribute
+    {
+        return Attribute::make(
+            get: fn() => $this->user_option_id != $this->correct_option_id
+        );
+    }
 
     /**
      * Get the question that owns the UserQuestion
